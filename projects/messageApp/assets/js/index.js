d681447c33a2c9742765e8c858eeb5e7b4d02d93
M4Y50N <pethersonreis@gmail.com>
2022-05-01 19:18:27 -0300

    update

--- projects/messageApp/assets/js/index.js
+++ projects/messageApp/assets/js/index.js
@@ -1,1 +1,16 @@
 var fs = require("fs");
+
+fs.readdir("./", (err, files) => {
+	if (err) throw err;
+	files
+		.filter((x) => {
+			return x.split(".")[1] == "txt";
+		})
+		.forEach((file, index) => {
+			fs.rename(file, `NovoNome${index}.txt`, (err) => {
+				if (err) throw err;
+			});
+		});
+
+	console.log("Arquivos renomeados");
+});
