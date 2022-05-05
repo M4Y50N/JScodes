4c2a97183ab84d26f01ade856a04b8164755ae6a
M4Y50N <pethersonreis@gmail.com>
2022-05-05 16:42:45 -0300

    Install ejs and inicialize server client comunication

--- projects/messageApp/server.js
+++ projects/messageApp/server.js
@@ -1,9 +1,35 @@
+const path = require("path");
+const http = require("http");
 const express = require("express");
 
+const socketio = require("socket.io");
+const { render } = require("express/lib/response");
+const res = require("express/lib/response");
+
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const PORT = 2323;
+const PORT = 2323 || process.env.PORT;
 
-app.listen(PORT, () => {
+//Use statics folders
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+//Load the initial page
+app.use("/", (req, res) => {
+	res.render("./html/login.html");
+});
+
+//User connects
+io.on("connection", (socket) => {
+	console.log("New ws connection");
+
+	socket.emit("message", "Bem vindo ao Chat!");
+});
+
+server.listen(PORT, () => {
 	console.log("Server running in port", PORT);
 });
